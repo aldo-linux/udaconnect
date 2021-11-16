2810cb804be536be27385b1e63ec7e43e3f14b93
Aldo Gonzalez <aldo.linux@gmail.com>
2021-11-15 20:39:25 -0500

    Added screenshots

--- modules/persons-grpc/app/grpcclient.py
+++ modules/persons-grpc/app/grpcclient.py
@@ -2,9 +2,7 @@ import grpc
 import person_pb2
 import person_pb2_grpc
 
-
-
-print("Sending sample payload...")
+print("Calling Persons gRPC...")
 
 channel = grpc.insecure_channel("localhost:5005")
 stub = person_pb2_grpc.PersonServiceStub(channel)
