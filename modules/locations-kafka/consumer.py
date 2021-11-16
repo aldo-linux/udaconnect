8ef9d575dd79c229551eb06a411157a87c0cb185
Aldo Gonzalez <aldo.linux@gmail.com>
2021-11-15 19:02:40 -0500

    bootstraps_servers set

--- modules/locations-kafka/consumer.py
+++ modules/locations-kafka/consumer.py
@@ -2,6 +2,6 @@ from kafka import KafkaConsumer
 
 TOPIC_NAME = 'locations'
 
-consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers='localhost:9092')
+consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers='kafka-headless:9092')
 for message in consumer:
     print (message)
